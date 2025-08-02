957d01dee824b134495abb963338f2f3c4bdd541
darkfriend <maslov.tlt@gmail.com>
2025-08-02 22:19:42 +0300

    remove protestware for RU/BY. Fuck you @limonte!

--- src/utils/dom/init.js
+++ src/utils/dom/init.js
@@ -122,7 +122,7 @@ const setupRTL = (targetElement) => {
 }
 
 /**
- * Add modal + backdrop + no-war message for Russians to DOM
+ * Add modal + backdrop + without no-war message for Russians to DOM
  *
  * @param {SweetAlertOptions} params
  */
